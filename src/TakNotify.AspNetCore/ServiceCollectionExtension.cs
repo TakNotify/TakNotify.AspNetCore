e025e1949690a440e2679beb6bcc25b4a2749025
Frandi Dwijayanto <dev@rumahfrandi.com>
2020-04-08 10:14:46 +0700

    Update to TakNotify v1.1.0 (#1)

--- src/TakNotify.AspNetCore/ServiceCollectionExtension.cs
+++ src/TakNotify.AspNetCore/ServiceCollectionExtension.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Frandi Dwi 2020. All rights reserved.
 // Licensed under the MIT License.
 using Microsoft.Extensions.DependencyInjection;
-using System.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace TakNotify
 {
@@ -18,13 +18,9 @@ namespace TakNotify
         /// <returns></returns>
         public static NotificationBuilder AddTakNotify(this IServiceCollection services)
         {
-            if (!services.Any(sc => sc.ServiceType == typeof(INotificationProviderFactory)))
-                services.AddSingleton<INotificationProviderFactory>(NotificationProviderFactory.GetInstance());
+            services.AddSingleton(sp => Notification.GetInstance(sp.GetRequiredService<ILogger<Notification>>()));
 
-            if (!services.Any(sc => sc.ServiceType == typeof(INotification)))
-                services.AddTransient<INotification, Notification>();
-
-            return new NotificationBuilder(services, NotificationProviderFactory.GetInstance());
+            return new NotificationBuilder(services);
         }
     }
 }
